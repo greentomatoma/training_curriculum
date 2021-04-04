0c9008ddad81fd7f68915df0d5299fa349ac85f1
greentomatoma <greentomatoma@gmail.com>
2021-04-04 17:00:33 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,17 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      # 曜日に対応した数値を取得　例）日曜日...0
+      wday_num = Date.today.wday
+      # 今日の曜日から１つずつ増加
+      wday_num += x
+      #「wday_numが7以上の場合」という条件式
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
       @week_days.push(days)
     end
 
